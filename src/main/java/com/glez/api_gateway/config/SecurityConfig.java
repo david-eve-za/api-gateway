3898d025486b21d55ccc90d8ed3b98d3089378ed
David Gonzalez <david.eve.za@gmail.com>
2023-10-12 18:02:36 -0500

    Api gateway first commit

---
+++ src/main/java/com/glez/api_gateway/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.glez.api_gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain filterChain(ServerHttpSecurity http) {
+        return http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(custom->custom
+                        .pathMatchers("/eureka/**","/actuator/**").permitAll()
+                        .anyExchange().authenticated())
+                .oauth2ResourceServer(oauth -> oauth.jwt(Customizer.withDefaults()))
+                .build();
+    }
+
+}
